f42da9dd0deb1f03ffffcf812ee147c837987196
Lindsay <lindsayywardd@gmail.com>
2024-07-03 12:34:22 -0400

    First Iter. of Portfolio Website

---
+++ README.md
@@ -0,0 +1,11 @@
+# My Web Portfolio
+
+Welcome to my portfolio website! This repository hosts a showcase of my projects, skills, and experience as a software developer.
+
+# Click the link below to view portfolio:
+
+### insert link
+
+
+
+This is my first portfolio, made with HTML, CSS, and JS. To be rebuilt with React and Node.js.
---
+++ script.js
@@ -0,0 +1,47 @@
+function goToGithub() {
+  window.open('https://github.com/lindsaywardd', '_blank');
+}
+
+function goToProject1() {
+  window.open('https://lindsaywardd.github.io/meal-planner-website/', '_blank');
+}
+
+function goToProject2() {
+    window.open('https://github.com/lindsaywardd?tab=repositories', '_blank');
+  }
+
+  function goToProject3() {
+    window.open('https://lindsaywardd.github.io/Stylin-Wiring-Jewelry-Web/', '_blank');
+  }
+
+function viewResume() {
+    const resumeUrl = 'https://github.com/lindsayward/lindsayward-web-portfolio/imgs/resume.pdf.pdf'; // Replace with resume URL
+    window.location.href = resumeUrl;
+}
+// so unnecessary lol
+function goToContactPage() {
+  window.open('contact.html', '_self');
+}
+
+// contact form
+document.addEventListener("DOMContentLoaded", function() {
+  // Initialize EmailJS with your public key
+  emailjs.init("iljAuqz0PtlPbAkAL");
+
+  // Form submission handler
+  const contactForm = document.getElementById('contact-form');
+  if (contactForm) {
+      contactForm.addEventListener('submit', function(event) {
+          event.preventDefault();
+
+          emailjs.sendForm('service_z2b4uod', 'template_0a4l11x', this)
+              .then(function(response) {
+                  console.log('Success!', response.status, response.text);
+                  alert('Message sent successfully!');
+              }, function(error) {
+                  console.error('Failed to send message:', error);
+                  alert('Failed to send message. Please try again later.');
+              });
+      });
+  }
+});
